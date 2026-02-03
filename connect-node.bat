7354bae84f04300841802f85cb7a86fb634e920a
Jarvis AI <jarvis@clawos.xyz>
2026-02-03 20:47:19 +0000

    Clean up non-ClawOS files

--- connect-node.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ==========================================
-echo    JARVIS NODE - CONNECT MODE
-echo ==========================================
-echo.
-echo Bu pencereyi KAPATMAYIN!
-echo Node baglandiginda otomatik kapanacak...
-echo.
-clawdbot node run --name="ozi-windows" --accept-commands --accept-cameras --accept-screens --accept-location
